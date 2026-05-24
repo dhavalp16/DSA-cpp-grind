480662e1918418c4ecabe52906a9b78b63bef70c
dhavalp16 <dhavaljpanchal1602@gmail.com>
2026-05-24 12:44:35 +0530

    Implemented Selection Sort

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -25,8 +25,25 @@ void bubbleSort(int arr[], int n) {
 	printArray(arr, n);
 }
 
+//Selection Sort
+void selectionSort(int arr[], int n) {
+	for (int i = 0;i < n - 1;i++) {
+		int smallestIdx = i;
+		for (int j = i+1;j < n;j++) {
+			if (arr[j] < arr[smallestIdx]) {
+				smallestIdx = j;
+			}
+		}
+		if (smallestIdx != i) {
+			std::swap(arr[i], arr[smallestIdx]);
+		}
+	}
+	printArray(arr, n);
+}
+
 int main() {
 	int arr[] = { 4, 1, 5, 3, 2 };
-	bubbleSort(arr, std::size(arr));
+	//bubbleSort(arr, std::size(arr));
+	selectionSort(arr, std::size(arr));
 	return 0;
 }
